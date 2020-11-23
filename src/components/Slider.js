850582a6af55578d086f57253a8e56ab6e932c1c
Rhythm Patel <prhythm3099@gmail.com>
2020-11-23 19:01:13 +0530

    add image slider in homepage

---
+++ src/components/Slider.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+import '../css/components/slider.css';
+
+import Img1 from '../static/images/slider_images/slider1.jpg';
+import Img2 from '../static/images/slider_images/slider2.jpg';
+
+import Arrow_left from '../static/images/slider_images/arrow_left.png';
+import Arrow_right from '../static/images/slider_images/arrow_right.png';
+
+class Slider extends Component {
+
+    state = {index: 0}
+
+    arrowClickHandler = () => {
+        let currentIndex = this.state.index;
+        this.setState({index: currentIndex+1});
+    }
+
+    getIndicators = () => {
+        let indicators = null;
+        if(this.state.index%2) {    // right image
+            indicators = (
+                <div className="indicator-wrapper">
+                    <div className="indicator" onClick={this.arrowClickHandler}></div>
+                    <div className="indicator indicator-on"></div>
+                </div>
+            )
+        } else {                    // left image
+            indicators = (
+                <div className="indicator-wrapper">
+                    <div className="indicator indicator-on"></div>
+                    <div className="indicator" onClick={this.arrowClickHandler}></div>
+                </div>
+            )
+        }
+        return indicators;
+    }
+    
+    render() {
+        const images = [Img1, Img2];
+        return(
+            <div className="slider">
+                <img src={images[this.state.index % 2]} alt="background image" className="slider-img"/>
+                <div className="slider-arrows">
+                    <img src={Arrow_left} alt="arrow left" className="slider-arrow left" onClick={this.arrowClickHandler}/>
+                    <img src={Arrow_right} alt="arrow right" className="slider-arrow right" onClick={this.arrowClickHandler}/>
+                </div>
+                {this.getIndicators()}
+            </div>
+        )
+    }
+}
+export default Slider;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Slider from '../components/Slider';
 export default class Homepage extends Component {
     render() {
         return (
             <div>
-                homepage
+                <Slider />
             </div>
         )
     }
