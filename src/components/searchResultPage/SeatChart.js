f79391e896ed1819d66596bf9a928e67f7beebce
Rhythm Patel <prhythm3099@gmail.com>
2020-12-01 19:34:45 +0530

    remove problematic comments

--- src/components/searchResultPage/SeatChart.js
+++ src/components/searchResultPage/SeatChart.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {Modal, Button} from 'react-bootstrap';
 
 import '../../css/components/searchResultPage/seatChart.css';
 
